9012c5bd210209ecf32f45b89980165ca541d8b5
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2025-04-02 15:58:46 -0400

    added material theme for dark and light modes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButton} from '@angular/material/button';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, MatButton, NgClass],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'browser-extension-ui';
+  isDark = true;
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    document.body.classList.toggle('dark-theme', this.isDark);
+    document.body.classList.toggle('light-theme', !this.isDark);
+  }
 }
