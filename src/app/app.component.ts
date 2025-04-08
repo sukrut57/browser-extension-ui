5893e28b094691c208bb04a7ebdec84801c15e8b
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2025-04-08 15:55:11 -0400

    added theming to toggle light and dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,16 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatButton} from '@angular/material/button';
-import {NgClass} from '@angular/common';
+
+import {NavbarComponent} from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule, MatButton, NgClass],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'browser-extension-ui';
-  isDark = true;
 
-  toggleTheme() {
-    this.isDark = !this.isDark;
-    document.body.classList.toggle('dark-theme', this.isDark);
-    document.body.classList.toggle('light-theme', !this.isDark);
-  }
 }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatButton } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIcon, MatIconRegistry} from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [MatSlideToggleModule, MatButton, MatToolbarModule, MatIcon],
+  templateUrl: './navbar.component.html',
+  standalone: true,
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  isDark = true;
+
+  constructor(private matIconRegistry: MatIconRegistry,
+              private domSanitizer: DomSanitizer) {
+
+   this.matIconRegistry.addSvgIcon(
+     'logo-dark',
+     this.domSanitizer.bypassSecurityTrustResourceUrl('/images/logo-dark-theme.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'logo-light',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('/images/logo-light-theme.svg')
+    )
+  }
+
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    document.body.classList.toggle('dark-theme', this.isDark);
+    document.body.classList.toggle('light-theme', !this.isDark);
+  }
+}
