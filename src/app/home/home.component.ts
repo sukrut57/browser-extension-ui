af17f91d3f79e0f597cda4a2eb81daaac6ad83ab
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2025-04-16 15:58:36 -0400

    chanes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,16 +5,19 @@ import {ExtensionService} from '../services/extension.service';
 import {
   MatCardModule,
 } from '@angular/material/card';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
+import {FormsModule} from '@angular/forms';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule,MatCardModule],
+  imports: [MatButtonModule, MatCardModule, MatSlideToggle, FormsModule, NgClass],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  isEnabled = true;
   extensions: ExtensionModel[] = [];
 
   constructor(private extensionService: ExtensionService) {
