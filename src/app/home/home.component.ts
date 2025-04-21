7222a7a0180a59e05250f28d0f214a988555284e
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2025-04-21 13:47:26 -0400

    added styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {ExtensionModel} from '../model/Extension.model';
 import {ExtensionService} from '../services/extension.service';
@@ -7,20 +7,20 @@ import {
 } from '@angular/material/card';
 import {MatSlideToggle} from '@angular/material/slide-toggle';
 import {FormsModule} from '@angular/forms';
-import {NgClass} from '@angular/common';
+import {NgClass, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, MatCardModule, MatSlideToggle, FormsModule, NgClass],
+  imports: [MatButtonModule, MatCardModule, MatSlideToggle, FormsModule, NgClass, NgStyle],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  isEnabled = true;
+export class HomeComponent  {
   extensions: ExtensionModel[] = [];
 
   constructor(private extensionService: ExtensionService) {
     this.extensions = this.extensionService.getExtensions();
   }
+
 }
